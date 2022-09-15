85abdb34a0eb52a4e66b1e35092e759e7c47dbbf
Jae Gifford <jae.giff@outlook.com>
2022-09-15 17:12:30 +0800

    attacks may not land on the same spot.

--- src/players/player.js
+++ src/players/player.js
@@ -3,6 +3,7 @@ The game is played against the computer, so make ‘computer’ players capable 
  The AI does not have to be smart, but it should know whether or not a given move is legal.
  (i.e. it shouldn’t shoot the same coordinate twice). */
 import Gameboards from '../objects/gameboard.js';
+import Ship from '../objects/ships.js';
 
 class Players {
   constructor(name = 'Computer') {
@@ -10,15 +11,16 @@ class Players {
   }
   attack(coordinates, enemyBoard) {
     // checks that an attack has not been previously made
-    if (enemyBoard.recordAttacks === undefined) {
+
+    if (enemyBoard.recordAttack.length === 0) {
       enemyBoard.receiveAttack(coordinates);
       return true;
     }
-    for (let i = 0; i < enemyBoard.recordAttacks.length; i++) {
+    for (let i = 0; i < enemyBoard.recordAttack.length; i++) {
       if (
         !(
-          coordinates[0] === enemyBoard.recordAttacks[i][0] &&
-          coordinates[1] === enemyBoard.recordAttacks[i][1]
+          coordinates[0] === enemyBoard.recordAttack[i][0] &&
+          coordinates[1] === enemyBoard.recordAttack[i][1]
         )
       ) {
         enemyBoard.receiveAttack(coordinates);
--- src/players/player.test.js
+++ src/players/player.test.js
@@ -2,13 +2,15 @@ import Players from './player.js';
 import Gameboards from '../objects/gameboard.js';
 import Ship from '../objects/ships.js';
 
+/* 
 let player = new Players('Jae');
 let cpuPlayer = new Players();
-
 let playerGameboard = new Gameboards();
 let cpuGameboard = new Gameboards();
-
+*/
 test('Player attack registers', () => {
+  let player = new Players('Jae');
+  let cpuGameboard = new Gameboards();
   let battleShip = new Ship(3);
   cpuGameboard.addShip(battleShip, [
     [0, 0],
@@ -18,7 +20,10 @@ test('Player attack registers', () => {
   player.attack([0, 0], cpuGameboard);
   expect(battleShip.getHitCoordinates()).toStrictEqual([[0, 0]]);
 });
+
 test('Player attacks the same spot fails', () => {
+  let player = new Players('Jae');
+  let cpuGameboard = new Gameboards();
   let battleShip = new Ship(3);
   cpuGameboard.addShip(battleShip, [
     [0, 0],
