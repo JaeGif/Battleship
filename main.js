d0b1b4f34f764305e102e0678162459bf993e53e
Jae Gifford <jae.giff@outlook.com>
2022-09-18 14:50:24 +0800

    attack individual coords added

--- main.js
+++ main.js
@@ -1,16 +1,10 @@
 import Gameboards from '../src/objects/gameboard.js';
-import { gameLoop, newGame, GameState } from '../src/gameloop.js';
+import { newGame, GameState } from '../src/gameloop.js';
 import Ship from '../src/objects/ships.js';
 import Players from '../src/players/player.js';
 import { createBoards } from '../src/dom/ui.js';
 
-const playerBoard = new Gameboards();
-const opponentBoard = new Gameboards();
-const humanPlayer = new Players('Jae');
-const computerPlayer = new Players();
-
 const game = (() => {
-  newGame(playerBoard, opponentBoard); // contains placing functions placed by default for now
+  newGame(); // contains placing functions placed by default for now
   createBoards();
-  gameLoop(playerBoard, opponentBoard, humanPlayer, computerPlayer);
 })();
